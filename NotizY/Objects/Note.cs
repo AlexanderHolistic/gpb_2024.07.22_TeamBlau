fb0631121aa71bd1a0cdc5e465aa78586d2bb94f
Paule1511 <astpauladolf@gmail.com>
2024-07-24 20:35:12 +0200

    Notiz und Notiz Datei Klasse.

---
+++ NotizY/Objects/Note.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using static System.Windows.Forms.LinkLabel;
+
+namespace NotizY.Objects
+{
+    internal class Note
+    {
+        public string content;
+        public string title;
+        public bool favourit;
+        public string group;
+
+        public Note(string title, string content, bool? favourit, string? group) {
+            this.title = title;
+            this.content = content;
+            this.favourit = favourit != null ? (bool)favourit : false;
+            this.group = group != null ? group : string.Empty;
+        }
+
+        public Note(string csvString)
+        {
+            string[] objects = csvString.Split(';');
+            this.title = objects[0];
+            this.content = objects[1];
+            this.favourit = Convert.ToBoolean(objects[2]);
+            this.group = objects[3];
+        }
+
+        public override string ToString()
+        {
+            return title;
+        }
+
+        public string toCsv()
+        {
+            return string.Format("{0};{1};{2};{3}", title, content, favourit, group);
+        }
+    }
+}
---
+++ NotizY/Objects/NoteFile.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace NotizY.Objects
+{
+    internal class NoteFile
+    {
+        string path; 
+        public NoteFile(string path) {
+            this.path = path;
+        }
+
+        public void loadNotes(List<Note> noteList)
+        {
+            try
+            {
+                noteList.Clear();
+                using(StreamReader sr = new StreamReader(path))
+                {
+                    while(!sr.EndOfStream)
+                    {
+                        Note note = new Note(sr.ReadLine());
+                        noteList.Add(note);
+                    }
+                }
+            }catch (Exception ex) {  MessageBox.Show("Error. Could not Load Notes\n" + ex.Message); }
+        }
+
+        public bool saveNotes(List<Note> noteList)
+        {
+            try
+            {
+                using(StreamWriter sw = new StreamWriter(path))
+                {
+                    foreach(Note note in noteList)
+                    {
+                        sw.WriteLine(note.toCsv());
+                    }
+                }
+                return true;
+            }catch (Exception ex) { MessageBox.Show("Error. Could not Save Notes\n" + ex.Message); }
+            return false;
+        }
+    }
+}
